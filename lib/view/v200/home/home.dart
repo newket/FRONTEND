a84d7cd8be35c2a1560388ce06bb4fdee15afbe0
sominyun <somin455@naver.com>
2024-10-18 19:06:05 +0900

    feat: 애플로그인

---
+++ lib/view/v200/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeV2 extends StatefulWidget {
+  const HomeV2({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomeV1();
+}
+
+class _HomeV1 extends State<HomeV2> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 로딩 중일 때 로딩 화면을 표시
+    return Scaffold();
+  }
+}
---
+++ lib/view/v200/my_ticket/my_ticket.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyTicketV2 extends StatefulWidget {
+  const MyTicketV2({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyTicketV2();
+}
+
+class _MyTicketV2 extends State<MyTicketV2> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 로딩 중일 때 로딩 화면을 표시
+    return Scaffold();
+  }
+}
