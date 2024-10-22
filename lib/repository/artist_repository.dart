72ca5a2055a8315cbdd9c34e2efd1ca1d0585e6f
sominyun <somin455@naver.com>
2024-10-23 01:14:12 +0900

    feat: 관심 아티스트 조회, 추가, 제거 api

--- lib/repository/artist_repository.dart
+++ lib/repository/artist_repository.dart
@@ -1,9 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:newket/config/amplitude_config.dart';
 import 'package:newket/model/artist_model.dart';
 import 'package:newket/secure/auth_dio.dart';
+import 'package:newket/view/v200/login/before_login.dart';
 
 class ArtistRepository {
   Future<SearchArtists> searchArtist(String keyword) async {
@@ -23,16 +26,54 @@ class ArtistRepository {
     await dio.post("/api/v1/artists/request?artistName=$artistName&deviceToken=$deviceToken");
   }
 
-  Future<SearchArtists> getFavoriteArtistV1s(BuildContext context) async {
+  Future<SearchArtists> getFavoriteArtists(BuildContext context) async {
     var dio = await authDio(context);
     final response = await dio.get("/api/v1/artists/favorite");
 
     return SearchArtists.fromJson(response.data);
   }
 
-  Future<void> putFavoriteArtistV1s(BuildContext context, FavoriteArtistV1s FavoriteArtistV1s) async {
+  Future<void> putFavoriteArtists(BuildContext context, FavoriteArtists favoriteArtists) async {
     var dio = await authDio(context);
-    final requestBody = FavoriteArtistV1s.toJson();
+    final requestBody = favoriteArtists.toJson();
     await dio.put("/api/v1/artists/favorite", data: requestBody);
+  }
+
+  Future<bool> getIsFavoriteArtist(int favoriteArtistId) async {
+    var dio = Dio();
+    const storage = FlutterSecureStorage();
+    final accessToken = await storage.read(key: 'ACCESS_TOKEN');
+    if (accessToken == null|| accessToken.isEmpty) {
+      return false;
+    } else {
+      dio.options.baseUrl = dotenv.get("BASE_URL");
+      dio.options.headers['Authorization'] = 'Bearer $accessToken';
+      final response = await dio.get("/api/v1/artists/favorite/$favoriteArtistId");
+      return response.data as bool;
+    }
+  }
+
+  Future<void> addFavoriteArtist(int artistId, BuildContext context) async {
+    var dio = Dio();
+    const storage = FlutterSecureStorage();
+    final accessToken = await storage.read(key: 'ACCESS_TOKEN');
+    if (accessToken==null || accessToken.isEmpty) {
+      AmplitudeConfig.amplitude.logEvent('BeforeLogin');
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const BeforeLogin(),
+        ),
+      );
+    } else {
+      dio.options.baseUrl = dotenv.get("BASE_URL");
+      dio.options.headers['Authorization'] = 'Bearer $accessToken';
+      await dio.put("/api/v1/artists/favorite/$artistId");
+    }
+  }
+
+  Future<void> deleteFavoriteArtist(int artistId, BuildContext context) async {
+    var dio = await authDio(context);
+    await dio.delete("/api/v1/artists/favorite/$artistId");
   }
 }
