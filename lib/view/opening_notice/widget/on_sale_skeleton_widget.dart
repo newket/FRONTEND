52d95f895148c7feeb8bdd5108914fda5cebc9bf
sominyun <somin455@naver.com>
2024-12-17 01:45:40 +0900

    fix:v2.0.4 on_sale_skeleton_widget padding 20 적용

--- lib/view/opening_notice/widget/on_sale_skeleton_widget.dart
+++ lib/view/opening_notice/widget/on_sale_skeleton_widget.dart
@@ -10,26 +10,29 @@ class OnSaleSkeletonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 20),
-        const SkeletonWidget(width: 244, height: 26),
-        const SizedBox(height: 8),
-        const SkeletonWidget(width: 192, height: 38),
-        const SizedBox(height: 24),
-        Column(
-          children: List.generate(
-            10,
-                (index) {
-              return Column(children: [
-                SkeletonWidget(width: MediaQuery.of(context).size.width - 40, height: 122),
-                const SizedBox(height: 12)
-              ]);
-            },
-          ),
-        )
-      ],
-       );
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const SkeletonWidget(width: 244, height: 26),
+          const SizedBox(height: 8),
+          const SkeletonWidget(width: 192, height: 38),
+          const SizedBox(height: 24),
+          Column(
+            children: List.generate(
+              10,
+                  (index) {
+                return Column(children: [
+                  SkeletonWidget(width: MediaQuery.of(context).size.width - 40, height: 122),
+                  const SizedBox(height: 12)
+                ]);
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
