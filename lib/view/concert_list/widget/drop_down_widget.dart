c4e69211f1264ede30168692116631c025e3b6f0
sominyun <somin455@naver.com>
2025-01-07 04:13:46 +0900

    feat:v2.1.0 드롭다운 패딩값 변경

--- lib/view/concert_list/widget/drop_down_widget.dart
+++ lib/view/concert_list/widget/drop_down_widget.dart
@@ -34,7 +34,7 @@ class _DropDownWidgetState extends State<DropDownWidget> {
     return Container(
       height: 34,
       width: 150,
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
       decoration: ShapeDecoration(
         color: backgroundColor, // 배경색 변경
         shape: RoundedRectangleBorder(
