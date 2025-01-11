731c9ca10d35fcffe750c9fad6c58a7c1b68fcbc
sominyun <somin455@naver.com>
2025-01-12 07:52:04 +0900

    feat:v2.1.0 font 상수화

---
+++ lib/constant/fonts.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+//body
+TextStyle? b8_14Med(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    letterSpacing: -0.42,
+  );
+}
+
+TextStyle? b8_14Bold(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.42,
+  );
+}
+
+TextStyle? b9_14Reg(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.42,
+  );
+}
+
+//caption
+TextStyle? c1_14Med(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.42,
+  );
+}
+
+TextStyle? c4_12Reg(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 12,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    letterSpacing: -0.36,
+  );
+}
