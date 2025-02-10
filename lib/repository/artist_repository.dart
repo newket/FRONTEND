84f758f1a7b8ff69c2a37abc05ddb2047d8509f7
sominyun <somin455@naver.com>
2025-02-10 23:24:01 +0900

    feat:v2.1.0 아티스트 프로필 api

--- lib/repository/artist_repository.dart
+++ lib/repository/artist_repository.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:newket/config/amplitude_config.dart';
+import 'package:newket/model/artist/artist_profile_model.dart';
 import 'package:newket/model/artist_model.dart';
 import 'package:newket/auth/auth_dio.dart';
 import 'package:newket/view/login/screen/before_login_screen.dart';
@@ -31,22 +32,22 @@ class ArtistRepository {
 
   //관심 아티스트 여부
   Future<bool> getIsFavoriteArtist(int favoriteArtistId, BuildContext context) async {
-    try{
+    try {
       var dio = await authDio(context);
       final response = await dio.get("/api/v1/artists/favorite/$favoriteArtistId");
       return response.data as bool;
-    }  on DioException {
+    } on DioException {
       return false;
     }
   }
 
   // 관심 아티스트 추가
   Future<bool> addFavoriteArtist(int artistId, BuildContext context) async {
-    try{
+    try {
       var dio = await authDio(context);
       await dio.put("/api/v1/artists/favorite/$artistId");
       return true;
-    }  on DioException {
+    } on DioException {
       AmplitudeConfig.amplitude.logEvent('BeforeLogin');
       Navigator.push(
         context,
@@ -62,5 +63,13 @@ class ArtistRepository {
   Future<void> deleteFavoriteArtist(int artistId, BuildContext context) async {
     var dio = await authDio(context);
     await dio.delete("/api/v1/artists/favorite/$artistId");
+  }
+
+  //아티스트 프로필
+  Future<ArtistProfileResponse> getArtistProfile(int artistId) async {
+    var dio = Dio();
+    dio.options.baseUrl = dotenv.get("BASE_URL");
+    final response = await dio.get("/api/v1/artists/$artistId");
+    return ArtistProfileResponse.fromJson(response.data);
   }
 }
