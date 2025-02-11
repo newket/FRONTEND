d73a81a935d765a26f29986c444f1cd0ae57c12b
sominyun <somin455@naver.com>
2025-02-11 21:47:41 +0900

    feat:v2.1.0 home 필요없는 코드 제거

--- lib/view/home/screen/home_screen.dart
+++ lib/view/home/screen/home_screen.dart
@@ -44,28 +44,15 @@ class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin 
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          titleSpacing: 0,
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  child: const Text(
-                    'NEWKET',
-                    style: TextStyle(
-                      color: pn_100,
-                      fontSize: 24,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  onTap: () {
-                    FocusScope.of(context).unfocus();
-                    FocusManager.instance.primaryFocus?.unfocus();
-                  },
-                ),
-              ],
+          titleSpacing: 20,
+          centerTitle: false,
+          title: const Text(
+            'NEWKET',
+            style: TextStyle(
+              color: pn_100,
+              fontSize: 24,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w800,
             ),
           ),
         ),
@@ -76,65 +63,62 @@ class _HomeScreen extends State<HomeScreen> with SingleTickerProviderStateMixin 
               FocusScope.of(context).unfocus();
               FocusManager.instance.primaryFocus?.unfocus();
             }, // 키보드 외부를 탭하면 키보드 숨기기
-            child: Stack(children: [
-              Positioned.fill(
-                  child: Column(children: [
-                Container(
-                  color: Colors.white,
-                  height: 44,
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TabBar(
-                    tabs: <Tab>[
-                      Tab(
-                        icon: SizedBox(
-                            width: MediaQuery.of(context).size.width / 2,
-                            height: 44,
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text("오픈 예정 티켓",
-                                      style: TextStyle(
-                                          fontFamily: 'Pretendard',
-                                          fontSize: 16,
-                                          color: controller.index == 0 ? pn_100 : f_40,
-                                          fontWeight: FontWeight.w600))
-                                ])),
-                      ),
-                      Tab(
-                        icon: SizedBox(
-                            width: MediaQuery.of(context).size.width / 2,
-                            height: 44,
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text("예매 중인 티켓",
-                                      style: TextStyle(
-                                          fontFamily: 'Pretendard',
-                                          fontSize: 16,
-                                          color: controller.index == 1 ? pn_100 : f_40,
-                                          fontWeight: FontWeight.w600))
-                                ])),
-                      ),
-                    ],
-                    controller: controller,
-                    dividerColor: Colors.transparent,
-                    // 흰 줄 제거
-                    indicatorColor: Colors.transparent,
-                    indicatorWeight: 2,
-                    indicatorPadding: const EdgeInsets.all(-11),
-                    // indicator 위치 내리기
-                    labelPadding: EdgeInsets.zero, //탭 크기가 안 작아지게
-                  ),
-                ),
-                Container(height: 5, color: f_10, width: double.infinity),
-                Expanded(
-                    child: TabBarView(
+            child: Column(children: [
+              Container(
+                color: Colors.white,
+                height: 44,
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: TabBar(
+                  tabs: <Tab>[
+                    Tab(
+                      icon: SizedBox(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: 44,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text("오픈 예정 티켓",
+                                    style: TextStyle(
+                                        fontFamily: 'Pretendard',
+                                        fontSize: 16,
+                                        color: controller.index == 0 ? pn_100 : f_40,
+                                        fontWeight: FontWeight.w600))
+                              ])),
+                    ),
+                    Tab(
+                      icon: SizedBox(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: 44,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text("예매 중인 티켓",
+                                    style: TextStyle(
+                                        fontFamily: 'Pretendard',
+                                        fontSize: 16,
+                                        color: controller.index == 1 ? pn_100 : f_40,
+                                        fontWeight: FontWeight.w600))
+                              ])),
+                    ),
+                  ],
                   controller: controller,
-                  children: const <Widget>[OpeningNoticeScreen(), OnSaleScreen()],
-                ))
-              ]))
+                  dividerColor: Colors.transparent,
+                  // 흰 줄 제거
+                  indicatorColor: Colors.transparent,
+                  indicatorWeight: 2,
+                  indicatorPadding: const EdgeInsets.all(-11),
+                  // indicator 위치 내리기
+                  labelPadding: EdgeInsets.zero, //탭 크기가 안 작아지게
+                ),
+              ),
+              Container(height: 5, color: f_10, width: double.infinity),
+              Expanded(
+                  child: TabBarView(
+                controller: controller,
+                children: const <Widget>[OpeningNoticeScreen(), OnSaleScreen()],
+              ))
             ])));
   }
 }
