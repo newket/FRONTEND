41b3413dcadb9f26a121c66a49db2a667a61db2d
sominyun <somin455@naver.com>
2025-02-26 21:43:10 +0900

    feat:v2.1.0 앱바 스크롤했을때 뒤에 색이 비치지 않도록, 하얀색 유지

--- lib/view/common/app_bar_back.dart
+++ lib/view/common/app_bar_back.dart
@@ -4,24 +4,26 @@ import 'package:newket/constant/colors.dart';
 
 PreferredSizeWidget appBarBack(BuildContext context, String title) {
   return AppBar(
-      leading: IconButton(
-          onPressed: () {
-            AmplitudeConfig.amplitude.logEvent('Back');
-            Navigator.pop(context); //뒤로가기
-          },
-          color: f_90,
-          icon: const Icon(Icons.arrow_back_ios_new_rounded)),
-      backgroundColor: Colors.white,
-      centerTitle: true,
-      title: Text(
-        title,
-        style: const TextStyle(
-          color: f_100,
-          fontSize: 16,
-          fontFamily: 'Pretendard',
-          fontWeight: FontWeight.w600,
-          height: 0.09,
-          letterSpacing: -0.48,
-        ),
-      ));
+    leading: IconButton(
+        onPressed: () {
+          AmplitudeConfig.amplitude.logEvent('Back');
+          Navigator.pop(context); //뒤로가기
+        },
+        color: f_90,
+        icon: const Icon(Icons.arrow_back_ios_new_rounded)),
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    title: Text(
+      title,
+      style: const TextStyle(
+        color: f_100,
+        fontSize: 16,
+        fontFamily: 'Pretendard',
+        fontWeight: FontWeight.w600,
+        height: 0.09,
+        letterSpacing: -0.48,
+      ),
+    ),
+    scrolledUnderElevation: 0,
+  );
 }
