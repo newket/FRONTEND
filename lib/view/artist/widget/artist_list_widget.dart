a76d0f662b5a5851a96e55de8938ed44d99775c3
sominyun <somin455@naver.com>
2025-02-26 21:45:21 +0900

    feat:v2.1.0 아티스트 리스트에 알림받기버튼 누름 여부 전달

--- lib/view/artist/widget/artist_list_widget.dart
+++ lib/view/artist/widget/artist_list_widget.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:newket/constant/colors.dart';
 import 'package:newket/constant/fonts.dart';
-import 'package:newket/model/ticket/search_result_model.dart';
-import 'package:newket/repository/artist_repository.dart';
+import 'package:newket/model/artist/artist_dto.dart';
+import 'package:newket/view/common/image_loading_widget.dart';
 import 'package:newket/view/search/widget/small_notification_button_widget.dart';
 
 class ArtistListWidget extends StatefulWidget {
-  final Artist artist;
+  final ArtistDto artist;
   final bool isFavoriteArtist;
   final double toastBottom;
+  final VoidCallback? onConfirm;
 
-  const ArtistListWidget({super.key, required this.artist, required this.isFavoriteArtist, required this.toastBottom});
+  const ArtistListWidget({super.key,
+    required this.artist,
+    required this.isFavoriteArtist,
+    required this.toastBottom,
+    this.onConfirm});
 
   @override
   State<StatefulWidget> createState() => _ArtistListWidget();
 }
 
 class _ArtistListWidget extends State<ArtistListWidget> {
-  late ArtistRepository artistRepository;
-  late final Artist artist;
-
   @override
   void initState() {
     super.initState();
-    artistRepository = ArtistRepository();
-    artist = widget.artist;
   }
 
   @override
@@ -37,35 +37,13 @@ class _ArtistListWidget extends State<ArtistListWidget> {
                 height: 48,
                 child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
                   Row(children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: Image.network(
-                        widget.artist.imageUrl ?? "",
-                        height: 48,
-                        width: 48,
-                        fit: BoxFit.cover,
-                        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                          if (loadingProgress == null) {
-                            return child; // 로딩이 완료되었을 때의 이미지
-                          }
-                          return Container(
-                            height: 48,
-                            width: 48,
-                            color: f_10, // 로딩 중일 때의 배경색
-                          );
-                        },
-                        errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                          return Container(
-                            height: 48,
-                            width: 48,
-                            color: f_10, // 로딩 실패 시의 배경색
-                          );
-                        },
-                      ),
-                    ),
+                    ImageLoadingWidget(width: 48, height: 48, radius: 12, imageUrl: widget.artist.imageUrl ?? ""),
                     const SizedBox(width: 12),
                     SizedBox(
-                      width: MediaQuery.of(context).size.width - 40 - 48 - 12 - 16 - 95,
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width - 40 - 48 - 12 - 16 - 95,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -91,7 +69,12 @@ class _ArtistListWidget extends State<ArtistListWidget> {
                       ),
                     )
                   ]),
-                  SmallNotificationButtonWidget(isFavoriteArtist: widget.isFavoriteArtist, artist: artist, toastBottom: widget.toastBottom,)
+                  SmallNotificationButtonWidget(
+                    isFavoriteArtist: widget.isFavoriteArtist,
+                    artist: widget.artist,
+                    toastBottom: widget.toastBottom,
+                    onConfirm: () => widget.onConfirm!(),
+                  )
                 ])),
             const SizedBox(height: 12),
           ],
