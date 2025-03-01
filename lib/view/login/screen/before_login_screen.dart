f2dd4d4c74caa433b6b5d1722a5b6d57b535aeb7
sominyun <somin455@naver.com>
2025-03-01 22:25:21 +0900

    feat:v2.1.0 bottom 버튼 제스처바 높이 고려

--- lib/view/login/screen/before_login_screen.dart
+++ lib/view/login/screen/before_login_screen.dart
@@ -23,23 +23,17 @@ class _BeforeLoginScreen extends State<BeforeLoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       //앱바
-      appBar: appBarBack(context, "로그인 후 기능 미리 보기"),
+      appBar: appBarBack(context, "미리보기"),
       backgroundColor: Colors.white,
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Image.asset(
-            'images/login/before_login.png',
-            width: 241,
-          )
-        ],
-      )),
+      body: Container(
+        width: double.infinity,
+          child:
+      Image.asset('images/login/before_login.png', width: 241, fit: BoxFit.cover)),
       bottomNavigationBar: Container(
         color: Colors.white,
         width: MediaQuery.of(context).size.width - 40,
-        height: 122,
-        padding: const EdgeInsets.only(bottom: 54, top: 12, left: 20, right: 20),
+        height: 88 + MediaQuery.of(context).viewPadding.bottom,
+        padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewPadding.bottom + 20, top: 12, left: 20, right: 20),
         child: ElevatedButton(
             onPressed: () async {
               AmplitudeConfig.amplitude.logEvent('BeforeLogin->Login');
@@ -48,13 +42,18 @@ class _BeforeLoginScreen extends State<BeforeLoginScreen> {
               await storage.deleteAll();
             },
             style: ElevatedButton.styleFrom(
-              elevation: 0, // 그림자 제거
-              backgroundColor: pn_100, // 버튼 배경색
+              elevation: 0,
+              // 그림자 제거
+              backgroundColor: pn_100,
+              // 버튼 배경색
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16),
               ),
-              padding: const EdgeInsets.symmetric(vertical: 16), // 상하 패딩
-              //fixedSize: Size(MediaQuery.of(context).size.width - 40, 56), // 고정 크기
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              // 상하 패딩
+              shadowColor: Colors.transparent,
+            ).copyWith(
+              splashFactory: NoSplash.splashFactory,
             ),
             child: const Text(
               '로그인 하러가기',
