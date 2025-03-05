2144d5f8dae191cc2a147866bdcb18789cedde79
sominyun <somin455@naver.com>
2025-03-05 22:43:21 +0900

    feat:v2.1.0 투명한 앱바 widget

---
+++ lib/view/common/app_bar_back_transparent.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:newket/config/amplitude_config.dart';
+import 'package:newket/constant/fonts.dart';
+
+PreferredSizeWidget appBarBackTransparent(BuildContext context, String title) {
+  return AppBar(
+    leading: IconButton(
+      onPressed: () {
+        AmplitudeConfig.amplitude.logEvent('Back');
+        Navigator.pop(context);
+      },
+      color: Colors.white,
+      icon: const Icon(Icons.arrow_back_ios_new_rounded),
+    ),
+    backgroundColor: Colors.transparent,
+    centerTitle: true,
+    title: Text(title, style: s1_16Semi(Colors.white)),
+    scrolledUnderElevation: 0,
+    elevation: 0.0,
+  );
+}
