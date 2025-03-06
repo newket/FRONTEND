5fcae695b9243bc93894534b92fdc0bb232a4bad
sominyun <somin455@naver.com>
2025-03-06 22:46:26 +0900

    feat:v2.1.0 font 추가

--- lib/constant/fonts.dart
+++ lib/constant/fonts.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
+//headline
+TextStyle? h2_24Semi(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 24,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    height: 1.42,
+    letterSpacing: -0.72,
+  );
+}
+
 //title
+TextStyle? t1_20Semi(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 20,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    height: 1.60,
+    letterSpacing: -0.60,
+  );
+}
+
 TextStyle? t2_18Semi(Color color) {
   return TextStyle(
     color: color,
@@ -25,12 +48,46 @@ TextStyle? s1_16Semi(Color color) {
 }
 
 //body
+TextStyle? b1_16Semi(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    height: 1.50,
+    letterSpacing: -0.48,
+  );
+}
+
+TextStyle? b6_16Med(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    height: 1.62,
+    letterSpacing: -0.48,
+  );
+}
+
+TextStyle? b7_16Reg(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 16,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    height: 1.62,
+    letterSpacing: -0.48,
+  );
+}
+
 TextStyle? b8_14Med(Color color) {
   return TextStyle(
     color: color,
     fontSize: 14,
     fontFamily: 'Pretendard',
     fontWeight: FontWeight.w500,
+    height: 1.57,
     letterSpacing: -0.42,
   );
 }
@@ -41,7 +98,8 @@ TextStyle? b8_14Bold(Color color) {
     fontSize: 14,
     fontFamily: 'Pretendard',
     fontWeight: FontWeight.w700,
-    letterSpacing: -0.42,
+    height: 1.50,
+    letterSpacing: -0.28,
   );
 }
 
@@ -51,6 +109,7 @@ TextStyle? b9_14Reg(Color color) {
     fontSize: 14,
     fontFamily: 'Pretendard',
     fontWeight: FontWeight.w400,
+    height: 1.57,
     letterSpacing: -0.42,
   );
 }
@@ -67,6 +126,17 @@ TextStyle? button2_14Semi(Color color) {
   );
 }
 
+TextStyle? button3_12Reg(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 12,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    height: 1.67,
+    letterSpacing: -0.36,
+  );
+}
+
 //caption
 TextStyle? c1_14Med(Color color) {
   return TextStyle(
@@ -75,6 +145,28 @@ TextStyle? c1_14Med(Color color) {
     fontFamily: 'Pretendard',
     fontWeight: FontWeight.w400,
     letterSpacing: -0.42,
+  );
+}
+
+TextStyle? c2_14Reg(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    height: 1.57,
+    letterSpacing: -0.42,
+  );
+}
+
+TextStyle? c3_12Med(Color color) {
+  return TextStyle(
+    color: color,
+    fontSize: 12,
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w500,
+    height: 1.67,
+    letterSpacing: -0.36,
   );
 }
 
